a16c580075f54c83ba6e93fdaab86b4269eb22db
Donald Harvey <donald@donaldharvey.co.uk>
2009-10-27 23:21:01 +0000

    Cleaned up screenshot.py a bit

--- src/snappy/ui/lin/screenshot.py
+++ src/snappy/ui/lin/screenshot.py
@@ -21,25 +21,25 @@ pygtk.require('2.0')
 import gtk
 from datetime import datetime, date, time
 class Screenshot:
-	
+	'''
+	This class contains mainly backend code to capture screenshots.
+	Its most important function is grab_area().
+	'''
 	def _get_window_title(self, window):
+		'''Get a gdk.Window's title'''
 		name = window.property_get('_NET_WM_NAME') #test this
 		if name:
 			return name[2]
 		return 'Untitled Window'
-	
+
 	def _get_active_window(self):
 		root = gtk.gdk.screen_get_default()
-		active = root.get_active_window()
-		return active
-	
-	def _get_current_window(self):
-		current_window = self._get_active_window()
+		current_window = root.get_active_window()
 		if not current_window:
 			current_window = gtk.gdk.window_at_pointer()
 		current_window = current_window.get_toplevel()
 		return current_window
-	
+
 	def grab_area(self, x, y, width, height):
 		#TODO: REWRITE API CODE!
 		#api.image.filename = "screenshot_" + datetime.now().strftime("%H-%M-%S_%d-%m-%y") + '.png'
@@ -55,18 +55,15 @@ class Screenshot:
 		#api.isvideo = False
 		#print "Screenshot saved to " + api.image.path + "."
 		return True
-	
+
 	def grab_window(self):
-		window = self._get_current_window()
+		window = self._get_active_window()
 		title = self._get_window_title(window)
 		print title
 		width, height = window.get_size()
 		pb = gtk.gdk.Pixbuf(gtk.gdk.COLORSPACE_RGB, False, 8, width, height)
 		pb = pb.get_from_drawable(window, window.get_colormap(), 0, 0, 0, 0, width, height)
 		pb.save('/home2/donald/.snappy/%s.png' % title, 'png')
-		
+
 #screenshot = Screenshot()
 #screenshot.grab_window()
-for window in gtk.gdk.window_get_toplevels():
-	print window
-
