50f1022534c89205b05a097fd662a6008cf2c8b2
Donald Harvey <donald@donaldharvey.co.uk>
2009-10-29 20:39:03 +0000

    Added password management to fileconfigmanager; also fixed inheritance for the ConfigManager and ConfigDict classes.

--- src/snappy/backend/configmanagers/configmanager.py
+++ src/snappy/backend/configmanagers/configmanager.py
@@ -1,14 +1,16 @@
 from UserDict import DictMixin
-class ConfigDict(DictMixin):
+class ConfigDict(DictMixin, object):
 	def __init__(self):
+		super(ConfigDict, self).__init__()
 		self.dict = dict()
 	def __getitem__(self, key):
 		self.dict.__getitem__(key)
 	def __setitem__(self, key, value):
 		self.dict.__setitem__(key, value)
 
-class ConfigManager:
+class ConfigManager(object):
 	def __init__(self):
+		super(ConfigManager, self).__init__()
 		from snappy.backend.urlproviders.trim import UrlProvider
 		self.urlprovider = UrlProvider()
 		self.settings = ConfigDict()
--- src/snappy/backend/configmanagers/fileconfigmanager.py
+++ src/snappy/backend/configmanagers/fileconfigmanager.py
@@ -1,7 +1,10 @@
 from configmanager import ConfigManager, ConfigDict
-from ConfigParser import ConfigParser
+from ConfigParser import ConfigParser, NoSectionError
 import sys
 import os
+from Crypto.Cipher import Blowfish #for password storage
+from hashlib import sha1
+from random import randrange
 
 def app_data_dir(appname='snappy'):
 	'''
@@ -26,33 +29,79 @@ def app_data_dir(appname='snappy'):
 
 class FileConfigManager(ConfigManager):
 	def __init__(self):
-		super(GnomeConfigManager, self).__init__()
+		super(FileConfigManager, self).__init__()
 		self.settings = FileConfigDict()
+
+	def _pad_pass(self, password):
+		pad_bytes = 8 - (len(password) % 8)
+		for i in range(pad_bytes - 1):
+			password += chr(randrange(0, 256))
+		# final padding byte; % by 8 to get the number of padding bytes
+		bflag = randrange(6, 248)
+		bflag -= bflag % 8 - pad_bytes
+		password += chr(bflag)
+		return password
+
+	def _depad_pass(self, password):
+		pad_bytes = ord(password[-1]) % 8
+		if not pad_bytes:
+			pad_bytes = 8
+		return password[:-pad_bytes]
+
+	def set_password(self, key, password):
+		cryptkey = sha1(key).hexdigest()
+		bf = Blowfish.new(cryptkey, AES.MODE_ECB)
+		encrypted_pass = bf.encrypt(self._pad_pass(password))
+		del password
+		self.settings[cryptkey] = encrypted_pass
+
+	def get_password(self, key):
+		cryptkey = sha1(key).hexdigest()
+		bf = Blowfish.new(cryptkey, AES.MODE_ECB)
+		encrypted_pass = self.settings[cryptkey]
+		decrypted_pass = self._depad_pass(bf.decrypt(encrypted_pass))
+		return decrypted_pass
 
 class FileConfigDict(ConfigDict):
 	def __init__(self):
 		super(FileConfigDict, self).__init__()
 		self.filename = os.path.join(app_data_dir(), 'settings.cfg')
+		if not os.path.exists(self.filename):
+			open(self.filename, 'w').close()
 		self.parser = ConfigParser()
-		self.parser.readfp(open(self.filename, 'r+'))
+		f = open(self.filename, 'r+')
+		self.parser.readfp(f)
+		f.close()
 
 	def __getitem__(self, key):
-		section, option = key.split('.', 1)
+		try:
+			section, option = key.split('.', 1)
+		except ValueError:
+			section, option = 'misc', key
 		return self.parser.get(section, option)
 
 	def __setitem__(self, key, value):
-		section, option = key.split('.', 1)
+		try:
+			section, option = key.split('.', 1)
+		except ValueError:
+			section, option = 'misc', key
 		if not self.parser.has_section(section):
 			self.parser.add_section(section)
 		self.parser.set(section, option, value)
+		f = open(self.filename, 'w')
+		self.parser.write(f)
+		f.close()
 
 	def __delitem__(self, key):
-		section, option = key.split('.', 1)
+		try:
+			section, option = key.split('.', 1)
+		except ValueError:
+			section, option = 'misc', key
 		self.parser.remove_option(section, option)
 
 	def keys(self):
-		keys = ()
+		keys = list()
 		for section in self.parser.sections():
 			for option in self.parser.items(section):
-				keys.append(section + '.' + option)
+				keys.append(section + '.' + option[0])
 		return keys
