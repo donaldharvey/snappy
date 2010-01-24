32d3680017fcfb8df8c717b80b3e5bc1d3da9d44
Donald Harvey <donald@donaldharvey.co.uk>
2010-01-24 00:13:06 +0000

    Added login ability to ImageShack uploads.

--- snappy/backend/httpstorage/imageshack.py
+++ snappy/backend/httpstorage/imageshack.py
@@ -1,7 +1,9 @@
 import urllib2
+from urllib import urlencode
 import httplib
 import urlparse
 import os
+import cookielib
 from snappy.backend.httpstorage import WebStorage
 from snappy.utils import MultipartDataHandler
 
@@ -9,14 +11,28 @@ class ImageShackStorage(WebStorage):
 	"""Uses ImageShack to store files online."""
 	nice_name = 'ImageShack online storage'
 	defaults = {
-		'imageshack.username': '',
+		'imageshack.email': '',
 		'imageshack.password': '',
 	}
 	def __init__(self, configmanager):
 		self.configmanager = configmanager
 
 	def store(self, filepath):
-		opener = urllib2.build_opener(MultipartDataHandler, urllib2.HTTPCookieProcessor)
+		email = self.configmanager['imageshack.email']
+		password = self.configmanager.get_password('imageshack.password')
+		cj = cookielib.CookieJar()
+		upload_opener = urllib2.build_opener(MultipartDataHandler, urllib2.HTTPCookieProcessor(cj))
+		if email and password:
+			login_opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+			result = login_opener.open('http://imageshack.us/auth.php', urlencode({
+				'username': email,
+				'password': password
+			}))
+			if result.read() != 'OK':
+				# Login error.
+				print 'Login error. Continuing anyway'
+				pass
+			result.close()
 		data = {
 			'MAX_FILE_SIZE': 13145728,
 			'refer': 'no_js_',
@@ -26,7 +42,7 @@ class ImageShackStorage(WebStorage):
 			'brand': '',
 			'fileupload': open(filepath)
 		}
-		result = opener.open('http://ufo.imageshack.us', data)
+		result = upload_opener.open('http://ufo.imageshack.us', data)
 		# Get the redirect location
 		new_location = result.url
 		return self._get_url_from_result_url(new_location)
